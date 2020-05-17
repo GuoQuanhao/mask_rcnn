83f98bb52873cd62cfddafd4512c18090ae14959
QuanHao Guo <49911294+GuoQuanhao@users.noreply.github.com>
2020-05-17 20:54:15 +0800

    Update json_to_dataset_new.py

--- json_to_dataset_new.py
+++ json_to_dataset_new.py
@@ -33,65 +33,67 @@ def main():
         out_dir = osp.join(osp.dirname(path_file_name[i]), out_dir)
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
-    # logger.warning('This script is aimed to demonstrate how to convert the '
-                   # 'JSON file to a single image dataset.')
-    # logger.warning("It won't handle multiple JSON files to generate a "
-                   # "real-use dataset.")
+        # logger.warning('This script is aimed to demonstrate how to convert the '
+                       # 'JSON file to a single image dataset.')
+        # logger.warning("It won't handle multiple JSON files to generate a "
+                       # "real-use dataset.")
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('json_file')
-    # parser.add_argument('-o', '--out', default=None)
-    # args = parser.parse_args()
+        # parser = argparse.ArgumentParser()
+        # parser.add_argument('json_file')
+        # parser.add_argument('-o', '--out', default=None)
+        # args = parser.parse_args()
 
-    # json_file = args.json_file
+        # json_file = args.json_file
 
-    # if args.out is None:
-        # out_dir = osp.basename(json_file).replace('.', '_')
-        # out_dir = osp.join(osp.dirname(json_file), out_dir)
-    # else:
-        # out_dir = args.out
-    # if not osp.exists(out_dir):
-        # os.mkdir(out_dir)
+        # if args.out is None:
+            # out_dir = osp.basename(json_file).replace('.', '_')
+            # out_dir = osp.join(osp.dirname(json_file), out_dir)
+        # else:
+            # out_dir = args.out
+        # if not osp.exists(out_dir):
+            # os.mkdir(out_dir)
 
-    # data = json.load(open(json_file))
-    # imageData = data.get('imageData')
+        # data = json.load(open(json_file))
+        # imageData = data.get('imageData')
 
-    if not imageData:
-        imagePath = os.path.join(os.path.dirname(json_file), data['imagePath'])
-        with open(imagePath, 'rb') as f:
-            imageData = f.read()
-            imageData = base64.b64encode(imageData).decode('utf-8')
-    img = utils.img_b64_to_arr(imageData)
+        if not imageData:
+            imagePath = os.path.join(os.path.dirname(json_file), data['imagePath'])
+            with open(imagePath, 'rb') as f:
+                imageData = f.read()
+                imageData = base64.b64encode(imageData).decode('utf-8')
+        img = utils.img_b64_to_arr(imageData)
 
-    label_name_to_value = {'_background_': 0}
-    for shape in sorted(data['shapes'], key=lambda x: x['label']):
-        label_name = shape['label']
-        if label_name in label_name_to_value:
-            label_value = label_name_to_value[label_name]
-        else:
-            label_value = len(label_name_to_value)
-            label_name_to_value[label_name] = label_value
-    lbl, _ = utils.shapes_to_label(
-        img.shape, data['shapes'], label_name_to_value
-    )
+        label_name_to_value = {'_background_': 0}
+        for shape in sorted(data['shapes'], key=lambda x: x['label']):
+            label_name = shape['label']
+            if label_name in label_name_to_value:
+                label_value = label_name_to_value[label_name]
+            else:
+                label_value = len(label_name_to_value)
+                label_name_to_value[label_name] = label_value
+        lbl, _ = utils.shapes_to_label(
+            img.shape, data['shapes'], label_name_to_value
+        )
 
-    label_names = [None] * (max(label_name_to_value.values()) + 1)
-    for name, value in label_name_to_value.items():
-        label_names[value] = name
+        label_names = [None] * (max(label_name_to_value.values()) + 1)
+        for name, value in label_name_to_value.items():
+            label_names[value] = name
 
-    lbl_viz = imgviz.label2rgb(
-        label=lbl, img=imgviz.asgray(img), label_names=label_names, loc='rb'
-    )
+        lbl_viz = imgviz.label2rgb(
+            label=lbl, img=imgviz.asgray(img), label_names=label_names, loc='rb'
+        )
 
-    PIL.Image.fromarray(img).save(osp.join(out_dir, 'img.png'))
-    utils.lblsave(osp.join(out_dir, 'label.png'), lbl)
-    PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, 'label_viz.png'))
+        PIL.Image.fromarray(img).save(osp.join(out_dir, 'img.png'))
+        utils.lblsave(osp.join(out_dir, 'label.png'), lbl)
+        PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, 'label_viz.png'))
 
-    with open(osp.join(out_dir, 'label_names.txt'), 'w') as f:
-        for lbl_name in label_names:
-            f.write(lbl_name + '\n')
+        with open(osp.join(out_dir, 'label_names.txt'), 'w') as f:
+            for lbl_name in label_names:
+                f.write(lbl_name + '\n')
 
-    logger.info('Saved to: {}'.format(out_dir))
+        print('INFO:Saved to: {}'.format('images/segmentation/test/' + file_name[i]))
+
+    print('INFO:finished!')
 
 
 if __name__ == '__main__':
